cadd456e3c765d366126a809f2b16eabc481d411
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-08-15 19:43:37 +0530

    fixed minor bugs

--- templates/app/ts-boiler.ts
+++ templates/app/ts-boiler.ts
@@ -13,7 +13,6 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.options('*', cors());
 app.use('/', routes);
 
 // routes
--- templates/app/ts-socket.ts
+++ templates/app/ts-socket.ts
@@ -22,7 +22,6 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.options('*', cors());
 app.use('/', routes);
 
 io.on('connection', (socket) => {
@@ -34,4 +33,4 @@ import healthRouter from "./routes/healthRoute"
 
 app.use("/api/v1/users", healthRouter)
 
-export default app;
+export default server;
