058d260aa5e1e38ee7dca30a3d4577f64291ab7e
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-12-05 22:18:53 +0530

    fix: added templates/send.tsx

--- src/templates/addons/smtp/base/infra/services/mail/templates/feedback/Send.tsx
+++ src/templates/addons/smtp/base/infra/services/mail/templates/feedback/Send.tsx
@@ -1,32 +1,32 @@
-import React from "react";
 import { Section, Text } from "@react-email/components";
+import React from "react";
 import BaseLayout from "../components/BaseLayout.js";
 
 void React;
 
 export default function FeedbackSentEmail({
-  title,
-  description,
-  sendBy,
-  projectName,
+	title,
+	description,
+	sendBy,
+	projectName,
 }: {
-  title: string;
-  description: string;
-  sendBy: string;
-  projectName: string;
+	title: string;
+	description: string;
+	sendBy: string;
+	projectName: string;
 }) {
-  return (
-    <BaseLayout projectName={projectName}>
-      <Section>
-        <Text style={style.topText}>{title}</Text>
-        <Text>{description}</Text>
-        <Text style={style.bottomText}>— {sendBy}</Text>
-      </Section>
-    </BaseLayout>
-  );
-};
+	return (
+		<BaseLayout projectName={projectName}>
+			<Section>
+				<Text style={style.topText}>{title}</Text>
+				<Text>{description}</Text>
+				<Text style={style.bottomText}>— {sendBy}</Text>
+			</Section>
+		</BaseLayout>
+	);
+}
 
 const style = {
-  topText: { fontSize: "18px", fontWeight: "bold" },
-  bottomText: { marginTop: "10px", fontStyle: "italic" }
+	topText: { fontSize: "18px", fontWeight: "bold" },
+	bottomText: { marginTop: "10px", fontStyle: "italic" },
 };
